3e5d6e9be016c70b8624e308576982dc5e5e768f
DOTHAIANH <97963238+DOTHAIANH@users.noreply.github.com>
2022-07-26 19:08:38 +0700

    commit

---
+++ FE/js/slideindex.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(".slide-show-skate").slick({
+        infinite: false,
+        slidesToShow:  1,
+        slidesToScroll: 1,
+        speed: 300,
+        autoplay: true,
+        autoplaySpeed: 4000,
+    });
+});
